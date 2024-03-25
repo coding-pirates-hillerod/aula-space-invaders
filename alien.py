e98d926aa83406a312586f6d931d9cd13cdbe87d
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-03-25 17:26:56 +0100

    Added initial alien setup

---
+++ alien.py
@@ -0,0 +1,13 @@
+from typing import Any
+import pygame
+
+
+class Alien(pygame.sprite.Sprite):
+    def __init__(self, x, y) -> None:
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("./img/alien2.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+    def update(self) -> None:
+        self.rect.y += 2
