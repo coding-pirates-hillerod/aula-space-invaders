60b8c07bbebdc74d3aa45ef6edd17ada3e87ffc3
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-03-27 14:49:44 +0100

    Fixed shooting error

--- bullet.py
+++ bullet.py
@@ -8,11 +8,11 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
 
-    def update(self, alien_group) -> None:
+    def update(self, group):
         self.rect.y -= 5
 
         if self.rect.bottom < 0:
             self.kill()
 
-        if pygame.sprite.spritecollide(self, alien_group, True):
+        if pygame.sprite.spritecollide(self, group, True):
             self.kill()
--- spaceship.py
+++ spaceship.py
@@ -9,9 +9,11 @@ class Spaceship(pygame.sprite.Sprite):
         self.image = pygame.image.load("./img/spaceship.png")
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
+        self.last_shot = pygame.time.get_ticks()
 
-    def update(self, screen_width, bullet_group) -> None:
+    def update(self, screen_width, bullet_group):
         speed = 4
+        cooldown = 250
 
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT] and self.rect.left > 0:
@@ -19,8 +21,11 @@ class Spaceship(pygame.sprite.Sprite):
         if key[pygame.K_RIGHT] and self.rect.right < screen_width:
             self.rect.x += speed
 
-        if key[pygame.K_SPACE]:
+        time_now = pygame.time.get_ticks()
+
+        if key[pygame.K_SPACE] and time_now - self.last_shot > cooldown:
             bullet = Bullet(self.rect.centerx, self.rect.top)
             bullet_group.add(bullet)
+            self.last_shot = time_now
 
         self.mask = pygame.mask.from_surface(self.image)
